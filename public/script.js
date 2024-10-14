a812ae60d9d186c8d519a12077b025e50875b1ef
Shihan Rishad <165176498+ShihanRishad@users.noreply.github.com>
2024-10-14 18:35:00 +0000

    UI improvements (Added mdw3)

--- public/script.js
+++ public/script.js
@@ -1,16 +1,36 @@
 const app = new TodoApp();
+function dom(query) { // Just do it faster 😂
+  return document.querySelector(query);
+}
+var listscroll = dom(".listscroll");
+let listheight;
+setTimeout(() => {
+  listscroll.scrollTo({
+    top: listscroll.scrollHeight,
+    behavior: 'smooth'
+  });
+  listheight = listscroll.scrollHeight;
+
+}, 500);
   
 function renderTasks() {
   const taskList = document.getElementById('taskList');
   taskList.innerHTML = '';
   app.getTasks().forEach((task, index) => {
-    const taskItem = document.createElement('li');
+    const taskItem = document.createElement('md-list-item');
     taskItem.className = 'task' + (task.completed ? ' completed' : '');
+    // let tasktype = taskItem.createAttribute("type");
+    // tasktype.value = "button";
+    taskItem.type = "button";
     taskItem.innerHTML = `
-      <span>${task.task}</span>
-      <span>
-        <button onclick="markAsCompleted(${index})">Complete</button>
-        <button onclick="removeTask(${index})">Remove</button>
+      <span slot="headline">${task.task}</span>
+      <span slot="end">
+            <md-icon-button onclick="markAsCompleted(${index})">
+              <md-icon><i class="material-symbols-rounded">check</i></md-icon>
+            </md-icon-button>
+            <md-icon-button onclick="removeTask(${index})">
+              <md-icon><i class="material-symbols-rounded">delete</i></md-icon>
+            </md-icon-button>
       </span>
     `;
     taskList.appendChild(taskItem);
@@ -24,6 +44,13 @@ function addTask() {
     document.getElementById('newTask').value = '';
     renderTasks();
   }
+
+setTimeout(() => {
+    listscroll.scrollTo({
+      top: listscroll.scrollHeight,
+      behavior: 'smooth'
+    });
+}, 100);
 }
 
 document.querySelector("#newTask").addEventListener('keydown', function(event) {
@@ -33,7 +60,10 @@ document.querySelector("#newTask").addEventListener('keydown', function(event) {
   });
 
 function removeTask(index) {
-  app.removeTask(index);
+  let sure = true
+  if (sure) {
+    app.removeTask(index);
+  }
   renderTasks();
 }
 
